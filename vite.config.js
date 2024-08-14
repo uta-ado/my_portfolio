ee52887d6486a41f715282e3fa9038ada1f0e5f7
jellinek <leGrandJellinek>
2024-08-14 21:46:16 +0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  publicPath: process.env.NODE_ENV === 'production'
+  base: process.env.NODE_ENV === 'production'
   ? '/my_portfolio/'
   : '/'
 })
